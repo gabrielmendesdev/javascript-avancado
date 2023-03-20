57addf4578d6d70e4edbe74a5c7ce523585560ab
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-03-20 13:56:47 -0300

    up

---
+++ aula05/ex12.js
@@ -0,0 +1,12 @@
+let alunos = [
+    {nome: "joão", idade: 15},
+    {nome: "josé", idade: 18},
+    {nome: "maria", idade: 20},
+    {nome: "tulio", idade:17}
+]
+
+let alunosDeMaior = alunos.filter(function(aluno) {
+    return aluno.idade >= 18
+})
+
+console.log(alunosDeMaior)
