81accab7a7e37b91d0131a900c81ecc4d62f3140
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-03-28 16:14:06 -0300

    up

---
+++ aula11/ex38.js
@@ -0,0 +1,9 @@
+//As vezes é mais fácil colar do vizinho
+
+function calculaPontencia(x = 2, y = x) {
+    console.log(Math.pow(x, y))
+}
+
+calculaPontencia()
+calculaPontencia(3)
+calculaPontencia(2, 3)
---
+++ aula11/ex39.js
@@ -0,0 +1,3 @@
+function parametroObrigatorioValid(parametro) {
+    throw new Error(`O parâmetro ${parametro} é obrigatório`)
+}
